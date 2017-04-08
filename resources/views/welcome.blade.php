370943952c9cd8d7d9a48d690f306c5fc04037ab
iamnick <89993281@qq.com>
2017-04-08 17:14:56 +0000

    Test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5 -By ThePushOn</div>
+                <div class="title">Laravel 5 -By zengjunlong</div>
             </div>
         </div>
     </body>

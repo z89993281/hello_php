f1b055f092e367a433b9f941eb10a2de548f0821
iamnick <89993281@qq.com>
2017-04-08 17:12:03 +0000

    PushOn

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5 -By iamnickTest</div>
+                <div class="title">Laravel 5 -By ThePushOn</div>
             </div>
         </div>
     </body>

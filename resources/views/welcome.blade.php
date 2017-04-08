837f8bd9ff704581da1472fb437403418ac4afdb
iamnick <89993281@qq.com>
2017-04-08 17:23:04 +0000

    test 1:23

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5 -By zengjunlong</div>
+                <div class="title">Laravel 5 -By zengjunlong 1:22</div>
             </div>
         </div>
     </body>

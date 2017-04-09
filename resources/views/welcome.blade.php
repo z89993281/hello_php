df6b197e48b07b859d6f1220a141bfcaa32424d1
iamnick <89993281@qq.com>
2017-04-09 01:20:59 +0000

    Good morning!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5 -By zengjunlong 1:22</div>
+                <div class="title">Good morning! Laravel 5 -By zengjunlong 1:22</div>
             </div>
         </div>
     </body>

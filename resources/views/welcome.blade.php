57bcb0d6193107d985a6b4d8df686f2fd8fb3f00
iamnick <89993281@qq.com>
2017-04-09 03:15:59 +0000

    fuxi Push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Good morning! Laravel 5 -By zengjunlong 1:22</div>
+                <div class="title">Hello Laravel 5 -By zengjunlong 11:15</div>
             </div>
         </div>
     </body>
